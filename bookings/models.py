d8952ef51afbd5d8af50df82ff6a9091188be9dc
Christina Hughes <c.j.hughes@hotmail.com>
2024-08-05 16:37:57 +0000

    Change default time zone to Europe/London

--- bookings/models.py
+++ bookings/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone # Pulls in default timezone info which is Europe/London
 
 # Create your models here.
 
@@ -16,7 +17,6 @@ class User(models.Model):
 # Model for table availability info
 class CafeTable(models.Model):
     id = models.AutoField(primary_key = True) # Unique table id
-
     num_of_seats = models.PositiveSmallIntegerField() # Number of seats at a table (4 seats total).
     #potentially add (validators=[MinValueValidator(0)]) at the end as field cannot be negative, but 0 would be allowed.
    
@@ -33,5 +33,5 @@ class Booking(models.Model):
     booking_time = models.TimeField() # Time when customer wants to book table - only in 1 hour slots from 6pm-6am
     num_of_guests = models.IntegerField() # Number of guests must be be min 1 and max 4
     special_requests = models.TextField() # Textfield so customers can add allergy info, birthday treats, etc.
-    booking_created_at = models.DateTimeField(auto_now_add=True, default=timezone.now) # Adds UK date & time booking was first made, can't be changed
-    booking_updated_at = models.DateTimeField(auto_now=True, default=timezone.now) # Adds UK date & time booking was amened, can be changed
+    booking_created_at = models.DateTimeField(auto_now_add=True, default=timezone.now) # Adds Europe/London date & time booking was first made, can't be changed
+    booking_updated_at = models.DateTimeField(auto_now=True, default=timezone.now) # Adds Europe/London date & time booking was amened, can be changed
