e249cba2f527abf3d6f1156c03ea76e84fa5065a
Christina Hughes <c.j.hughes@hotmail.com>
2024-08-05 16:33:52 +0000

    Add field types and definitions to field names in User, CafeTable, and Booking models

--- bookings/models.py
+++ bookings/models.py
@@ -9,15 +9,19 @@ class User(models.Model):
     first_name = models.CharField(max_length = 50) # User's first name, allows a max length of 50 characters 
     last_name = models.CharField(max_length = 50) # User's last name, allows a max length of 50 characters 
     email = models.EmailField(unique = True, max_length = 254) # Ensures User email address is used just once on the site & not too long
-    password = PasswordInput() # User password
+    password = models.CharField(max_length=128) # User password
     is_admin = models.BooleanField(default=False) # Indicates if User is admin or not. Default set to false.
 
 
 # Model for table availability info
 class CafeTable(models.Model):
     id = models.AutoField(primary_key = True) # Unique table id
-    num_of_seats = models.IntegerField() # Number of seats at a table (4 seats total)
-    is_available = models.BooleanField(default=True) # Indicates if a table is avliable or not. Default set to true.
+
+    num_of_seats = models.PositiveSmallIntegerField() # Number of seats at a table (4 seats total).
+    #potentially add (validators=[MinValueValidator(0)]) at the end as field cannot be negative, but 0 would be allowed.
+   
+    is_available = models.BooleanField(default=True) # Indicates if a table is available or not. Default set to true.
+   
 
 
 # Model for making a booking
@@ -29,5 +33,5 @@ class Booking(models.Model):
     booking_time = models.TimeField() # Time when customer wants to book table - only in 1 hour slots from 6pm-6am
     num_of_guests = models.IntegerField() # Number of guests must be be min 1 and max 4
     special_requests = models.TextField() # Textfield so customers can add allergy info, birthday treats, etc.
-    booking_created_at = models.DateTimeField(auto_now_add=True) # Adds date & time booking was made, can't be changed
-    booking_updated_at = models.DateTimeField(auto_now=True) # Adds date & time booking was amened, can be changed
+    booking_created_at = models.DateTimeField(auto_now_add=True, default=timezone.now) # Adds UK date & time booking was first made, can't be changed
+    booking_updated_at = models.DateTimeField(auto_now=True, default=timezone.now) # Adds UK date & time booking was amened, can be changed
