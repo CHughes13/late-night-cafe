4c08f38edce68793af8f8080caca2ad9e8a491fb
Christina Hughes <c.j.hughes@hotmail.com>
2024-08-08 18:14:35 +0000

    Create templates folder in bookings and add basic BookingsListAdmin info to views

--- bookings/views.py
+++ bookings/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.http import HttpResponse
+from django.views import generic
+from .models import Booking
 
+"""
+from django.views.generic import TemplateView use this when creating the HOMEPAGE
 class HomePage(TemplateView):
-    """
-    Displays home page"
-    """
+    # "Displays home page"
     template_name = 'index.html'
+"""
+class BookingsListAdmin(generic.ListView):
+    model = Booking
+    template_name = "bookings/bookings_list_admin.html"
+    context_object_name = "bookings_list_for_admin"
 
-def my_bookings(request):
-    return HttpResponse("Hello World!")
+
