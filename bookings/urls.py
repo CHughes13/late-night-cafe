5a5f0ca005ff6fba023512b024ffedcdd30e3211
Christina Hughes <c.j.hughes@hotmail.com>
2024-08-08 22:38:41 +0000

    Fix TemplateDoesNotExist error - templates folder missing s at the end

--- bookings/urls.py
+++ bookings/urls.py
@@ -11,13 +11,13 @@ urlpatterns = [
     path("sign_up/", views.SignUpView.as_view(), name="sign_up"),
 
     # Login Page
-    path("login/", LoginView.as_view(template_name = "registration/login.html"), name = "login" ),
+    path("login/", LoginView.as_view(template_name="registration/login.html"), name="login"),
 
     # Booking List for Admin Page
-    path("admin/bookings/", views.BookingListAdmin.as_view(), name = "admin_bookings"),
+    path("admin/bookings/", views.BookingListAdmin.as_view(), name="admin_bookings"),
 
     # Booking List for User Page
-    path("user/bookings/", views.BookingListUser.as_view(), name = "user_bookings"),
+    path("user/bookings/", views.BookingListUser.as_view(), name ="user_bookings"),
 
     # Make a Booking Page
     path("make_a_booking/", views.BookingCreateView.as_view(), name = "make_a_booking"),
