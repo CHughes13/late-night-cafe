316f05a134e779f99b58b2db86e8984786d7e90b
Christina Hughes <c.j.hughes@hotmail.com>
2024-08-08 19:19:54 +0000

    Add urlpatterns in urls.py. Pages for home, sign up, login, booking list Admin, BL User, and make a booking

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,6 +1,24 @@
 from . import views
 from django.urls import path
+from django.contrib.auth.views import LoginView  # Import Django's built-in login view
+
 
 urlpatterns = [
+    # Homepage
     path("", views.HomePage.as_view(), name="home"),
+
+    # Sign Up Page
+    path("sign_up/", views.SignUpView.as_view(), name="sign_up"),
+
+    # Login Page
+    path("login/", LoginView.as_view(template_name = "registration/login.html"), name = "login" ),
+
+    # Booking List for Admin Page
+    path("admin/bookings/", views.BookingListAdmin.as_view(), name = "admin_bookings"),
+
+    # Booking List for User Page
+    path("user/bookings/", views.BookingListUser.as_view(), name = "user_bookings"),
+
+    # Make a Booking Page
+    path("make_a_booking/", views.BookingCreateView.as_view(), name = "make_a_booking"),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,36 +1,39 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Booking
+from django.contrib.auth.views import LoginView  # Import Django's built-in login view
 
 
-# Homepage view
+# Homepage View
 class HomePage(generic.TemplateView):
     template_name = "index.html" # Do I need to change this to "home.html"?
 
 
-# Sign Up Page view
+# Sign Up Page View
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     template_name = "sign_up.html"
     success_url = reverse_lazy("login") # Redirects to the login page when sign up is successful
 
-# Login view
-# Django's built-in LoginView is used directly in urls.py
+
+# Login View = using Django's built-in LoginView
 
 
-# Bookings List for Admin view
-class BookingsListAdmin(generic.ListView):
+# Booking List for Admin View
+class BookingListAdmin(generic.ListView):
     model = Booking
     template_name = "bookings/bookings_list_admin.html"
     context_object_name = "bookings_list_for_admin"
 
-# Bookings List for User view
-class BookingsListUser(generic.ListView):
+
+# Booking List for User View
+class BookingListUser(generic.ListView):
     model = Booking
     template_name = "bookings/booking_list_user.html"
     context_object_name = "booking_list_for_user"
 
-# Make a Bookings view
+
+# Make a Booking View
 class BookingCreateView(generic.CreateView):
     model = Booking
     form_class = BookingForm
