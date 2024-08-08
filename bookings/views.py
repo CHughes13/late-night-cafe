c868cebddddcea1fe2a4ce58875c9c7d744b00ed
Christina Hughes <c.j.hughes@hotmail.com>
2024-08-08 18:58:01 +0000

    Add Views for Homepage, Sign Up Page, Login, Booking List for Admin, BL for User, Make a Booking

--- bookings/views.py
+++ bookings/views.py
@@ -2,15 +2,41 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Booking
 
-"""
-from django.views.generic import TemplateView use this when creating the HOMEPAGE
-class HomePage(TemplateView):
-    # "Displays home page"
-    template_name = 'index.html'
-"""
+
+# Homepage view
+class HomePage(generic.TemplateView):
+    template_name = "index.html" # Do I need to change this to "home.html"?
+
+
+# Sign Up Page view
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = "sign_up.html"
+    success_url = reverse_lazy("login") # Redirects to the login page when sign up is successful
+
+# Login view
+# Django's built-in LoginView is used directly in urls.py
+
+
+# Bookings List for Admin view
 class BookingsListAdmin(generic.ListView):
     model = Booking
     template_name = "bookings/bookings_list_admin.html"
     context_object_name = "bookings_list_for_admin"
+
+# Bookings List for User view
+class BookingsListUser(generic.ListView):
+    model = Booking
+    template_name = "bookings/booking_list_user.html"
+    context_object_name = "booking_list_for_user"
+
+# Make a Bookings view
+class BookingCreateView(generic.CreateView):
+    model = Booking
+    form_class = BookingForm
+    template_name = "bookings/booking_form.html"
+    success_url = "/" # Will redirect to homepage or another page after booking
+
+
 
 
