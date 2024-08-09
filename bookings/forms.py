4802019fbcd6477028f1e7509c1167eab96cf234
Christina Hughes <c.j.hughes@hotmail.com>
2024-08-09 00:24:27 +0000

    Update forms.py, add BookingForm validation

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm # Import Django's built-in UserCreationForm
-from django.contrib.auth.models import User # Import Django's built-in User
+from django.contrib.auth.models import User # Import Django's built-in User model
+from django.core.exceptions import ValidationError # Import used to raise validation errors in forms
 from .models import Booking # Import local Booking model
+import datetime # For handling date and time operations
 
 # Form users fill in to register and sign up to the site
-class UserCreationForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["first_name", "last_name", "email", "username", "password1", "password2"]
@@ -13,4 +15,13 @@ class UserCreationForm(UserCreationForm):
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ["booking date", "booking_time", "table_booked", "num_of_guests"]
+        fields = ["booking_date", "booking_time", "table_booked", "num_of_guests"]
+
+    # Form validation: if a user tries to pick a booking date in the past, they'll get immediate feedback.
+    def clean_booking_date(self): 
+        booking_date = self.cleaned_data.get("booking_date")
+        if booking_date < datetime.date.today():
+            raise ValidationError("The booking date cannot be in the past. Please select another option.")
+        return booking_date
+        
+
