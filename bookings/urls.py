9cae6745eb1baa2926a2d5a1e20730c7b34328cf
Christina Hughes <c.j.hughes@hotmail.com>
2024-08-12 02:16:59 +0000

    Changed from direct view imports to importing all views using from . import views

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,19 +3,21 @@ from django.urls import path
 from django.contrib.auth.views import LoginView  # Import Django's built-in login view
 
 #URL pattern order should go from the least to the most specific to ensure that only one path matches.
+
 urlpatterns = [
+    
     # This is what is shown on the Homepage
     path("", views.BookingList.as_view(), name="home"),
 
     # Page where User can create a new booking
-    path("create-booking/", BookingCreateView.as_view(), name="create_booking"),
+    path("create-booking/", views.BookingCreateView.as_view(), name="create_booking"),
 
     # User dashboard page where they can see all their bookings
-    path("dashboard/", BookingListView.as_view(), name="user_dashboard"),
+    path("dashboard/", views.BookingListView.as_view(), name="user_dashboard"),
 
-    # So User can updating an existing booking 
-     path('update-booking/<int:pk>/', BookingUpdateView.as_view(), name='update_booking'),
+    # So User can update an existing booking
+    path("update-booking/<int:pk>/", views.BookingUpdateView.as_view(), name="update_booking"),
 
-    # So User can delete an existing booking 
-    path('delete-booking/<int:pk>/', BookingDeleteView.as_view(), name='delete_booking'),
+    # So User can delete an existing booking
+    path("delete-booking/<int:pk>/", views.BookingDeleteView.as_view(), name="delete_booking"),
 ]
