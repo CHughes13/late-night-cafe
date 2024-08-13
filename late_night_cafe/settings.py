88b950e9d485f358d387a0fb6ae13437b491a2ec
Christina Hughes <c.j.hughes@hotmail.com>
2024-08-13 13:46:42 +0000

    Add django.contrib.messages to settings

--- late_night_cafe/settings.py
+++ late_night_cafe/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 import dj_database_url
 if os.path.isfile("env.py"):
     import env
