b3c23276ec893e8cf9f3a751fd356cf6dd5b34c3
Christina Hughes <c.j.hughes@hotmail.com>
2024-08-13 13:48:32 +0000

    Add message_tags to settings

--- late_night_cafe/settings.py
+++ late_night_cafe/settings.py
@@ -150,6 +150,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+MESSAGE_TAGS = {
+    messages.SUCCESS: "alert-success",
+    messages.ERROR: "alert-danger",
+}
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
