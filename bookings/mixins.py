feb1a48a5fabde97fa8e7c246011a565025a3152
Christina Hughes <c.j.hughes@hotmail.com>
2024-08-14 14:07:26 +0000

    Fix issue where admin couldn't amend other users bookings by giving a superuser all powerful permissions

--- bookings/mixins.py
+++ bookings/mixins.py
@@ -1,4 +1,3 @@
-from django.http import Http404
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.core.exceptions import PermissionDenied
 
@@ -7,15 +6,24 @@ If an unauthorised user tries to access a booking that they are not the
 owner of (or not a superuser/admin), they will be diverted to a 404 page.
 """
 
+class SuperuserRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_superuser
+
+    def handle_no_permission(self):
+        return self.redirect_to_login()
+
+
+
 class UserIsOwnerMixin:
     def dispatch(self, request, *args, **kwargs):
         booking = self.get_object()
 
-        # Allows superusers access any booking by any user
+        # Allow superusers to access any booking
         if request.user.is_superuser:
             return super().dispatch(request, *args, **kwargs)
 
-        # Otherwise, checks to see if the booking belongs to the current user
+        # Ensure that only the owner can access the booking
         if booking.user != request.user:
             raise PermissionDenied("You do not have permission to edit or delete this booking.")
 
