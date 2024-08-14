05f1f912563409821f5f1d2c9eff0ef3755e4d2f
Christina Hughes <c.j.hughes@hotmail.com>
2024-08-14 02:00:51 +0000

    Add form validation so a user can't select a booking date more than 1 year in advance

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm # Import Django's built-in UserCreationForm
-from django.contrib.auth.models import User # Import Django's built-in User model
-from django.core.exceptions import ValidationError # Import used to raise validation errors in forms
-from .models import Booking # Import local Booking model
-import datetime # For handling date and time operations
+from django.contrib.auth.forms import UserCreationForm  # Import Django's built-in UserCreationForm
+from django.contrib.auth.models import User  # Import Django's built-in User model
+from django.core.exceptions import ValidationError  # Import used to raise validation errors in forms
+from .models import Booking  # Import local Booking model
+import datetime  # For handling date and time operations
 
 # Form users fill in to register and sign up to the site
 class CustomUserCreationForm(UserCreationForm):
@@ -17,9 +17,18 @@ class BookingForm(forms.ModelForm):
         model = Booking
         fields = ["booking_date", "booking_time", "table_booked", "num_of_guests", "special_requests"]
 
-    # Form validation: if a user tries to pick a booking date in the past, they'll get immediate feedback.
-    def clean_booking_date(self): 
+    # Form validation: if a user tries to pick an invalid booking date, they'll get immediate feedback
+    def clean_booking_date(self):
         booking_date = self.cleaned_data.get("booking_date")
-        if booking_date < datetime.date.today():
-            raise ValidationError("The booking date cannot be in the past. Please select another option.")
+        today = datetime.date.today()
+        one_year_from_now = today + datetime.timedelta(days=365)
+
+        # Check if the booking date is in the past
+        if booking_date < today:
+            raise ValidationError("The booking date cannot be in the past. Please select another date.")
+        
+        # Check if the booking date is more than one year in advance
+        if booking_date > one_year_from_now:
+            raise ValidationError("Sorry, we don't accept bookings more than one year in advance. Please select another date.")
+        
         return booking_date
